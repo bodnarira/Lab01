14948a539f8c5bbb0eb2a7bfc613c86159cf6fe4
bodnarira <55149654+bodnarira@users.noreply.github.com>
2019-12-05 17:15:42 +0200

    update ISP

--- ISP.py
+++ ISP.py
@@ -1,15 +1,36 @@
-class IRect:
-    def DrawRect(self):
+class Phone():
+    def update_iphone():
+        pass
+    def update_android():
         pass
 
-class IShape:
-    def DrawShape(self):
+class IIphone:
+    def update_iphone(self):
         pass
 
-class Rect(IRect):
-    def DrawRect(self):
-        return "Rect"
+class IAndroid:
+     def update_android(self):
+        pass
 
-class Shape(IShape):
-    def DrawShape(self):
-        return "Shape"
+class Iphone(IIphone):
+    def update_iphone(self):
+        print("update to 13 version")
+        # we don't need this code
+        #def update_android(self):
+        #print("update to 9.0.1 version")
+        
+
+class Android(IAndroid):
+    def update_android(self):
+        print("update to 9.0.1 version")
+        # we don't need this code
+        #def update_iphone(self):
+        #print("update to 13 version")
+        
+
+if __name__ == "__main__"
+    android = Android()
+    iphone = Iphone()
+    android.update_android()
+    iphone.update_iphone()
+        
