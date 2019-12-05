5316a6304a0c1c28bf46d8813d5eba2e01698a0c
bodnarira <55149654+bodnarira@users.noreply.github.com>
2019-12-05 19:08:24 +0200

    update lab 08

--- Lab08.py
+++ Lab08.py
@@ -2,34 +2,33 @@ import os
 
 class Dir:
     def duplicates (path:str):
-        list_of_files = os.listdir(path)
+        self.list_of_files = os.listdir(path)
         i = 0
         result = []
-        for i in range(len(list_of_files)):
-            br = False
-            for j in range(len(result)):
-                for o in range(len(result[j])):
-                    if list_of_files[i] == result[j][o]:
-                        br = True
-                        break
-                if br:
-                    break
-            if br:
+        for i in range(len(self.list_of_files)):
+            if self._check_in_res_(result, i):
                 continue
-            with open(self.path + list_of_files[i], 'rb') as f:
+            with open(self.path + self.list_of_files[i], 'rb') as f:
                 main_file = f.readlines()
             added = False
-            for j in range(i+1, len(list_of_files)):
-                with open(self.path + list_of_files[j], 'rb') as f:
+            for j in range(i+1, len(self.list_of_files)):
+                with open(self.path + self.list_of_files[j], 'rb') as f:
                     check_file = f.readlines()
                 if (str(main_file) == str(check_file)):
                     if added == False:
                         result.append([])
-                        result[len(result)-1].append(list_of_files[i])
+                        result[len(result)-1].append(self.list_of_files[i])
                         added = True
-                    result[len(result)-1].append(list_of_files[j])
+                    result[len(result)-1].append(self.list_of_files[j])
 
         return result
+    
+    def _check_in_res_ (self, result, i):
+        for j in range(len(result)):
+            for o in range(len(result[j])):
+                if self.list_of_files[i] == result[j][o]:
+                    return true
+        return false
 
 if __name__ == '__main__':
     print(Dir.duplicates("../../Python/New/"))
