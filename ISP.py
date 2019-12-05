be81c39191a66dc6f1a6ba677c7d0f44cef05308
bodnarira <55149654+bodnarira@users.noreply.github.com>
2019-12-05 17:49:25 +0200

    Maybe it will be good :)

--- ISP.py
+++ ISP.py
@@ -1,49 +1,27 @@
-#class Phone():
-#    def update_iphone():
-#        pass
-#    def update_android():
-#        pass
-
-#class Iphone(Phone):
-#    def update_iphone():
-#        print("Updated to 13v.")
-#    def update_android():
-#       # ??? Iphone hasnt got android os
-
-#class Android(Phone):
-#    def update_iphone():
-#        # ??? Android hasnt got ios
-#    def update_android():
-#        print("Updated to 9.1v.")
-
-
 class IIphone:
     def update_iphone(self):
         pass
 
-class IAndroid:
-     def update_android(self):
+class PhoneCall:
+     def call(self):
         pass
 
-class Iphone(IIphone):
+class Iphone(PhoneCall, IIphone):
     def update_iphone(self):
         print("update to 13 version")
-        # we don't need this code
-        #def update_android(self):
-        #print("update to 9.0.1 version")
+    def call(self):
         
 
-class Android(IAndroid):
-    def update_android(self):
-        print("update to 9.0.1 version")
-        # we don't need this code
-        #def update_iphone(self):
-        #print("update to 13 version")
+class Android(PhoneCall):
+    def call(self):
+        print("I'm calling to you")
         
 
 if __name__ == "__main__"
     android = Android()
     iphone = Iphone()
-    android.update_android()
+    android.update_iphone()  #error
     iphone.update_iphone()
+    android.call()
+    iphone.call()
         
