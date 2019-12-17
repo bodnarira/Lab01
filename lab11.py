dda8229e31ba75b5b825596d3ab7443b423b407a
bodnarira <55149654+bodnarira@users.noreply.github.com>
2019-12-17 20:23:40 +0200

    lab 11

---
+++ lab11.py
@@ -0,0 +1,45 @@
+from contextlib import contextmanager
+from threading import Lock
+
+class RWLock:
+    def __init__(self):
+        self.w_lock = Lock()
+        self.r_lock = Lock()
+        self.r_count = 0
+
+    def r_acquire(self):
+        self.r_lock.acquire()
+        self.r_count += 1
+        if self.r_count == 1:
+            self.w_lock.acquire()
+        self.r_lock.release()
+
+    def r_release(self):
+        assert self.r_count > 0
+        self.r_lock.acquire()
+        self.r_count -= 1
+        if self.r_count == 0:
+            self.w_lock.release()
+        self.r_lock.release()
+
+    def w_acquire(self):
+        self.w_lock.acquire()
+
+    def w_release(self):
+        self.w_lock.release()
+
+    @contextmanager   # use with -WITH-
+    def r_locked(self):
+        try:
+            self.r_acquire()
+            yield
+        finally:
+            self.r_release()
+
+    @contextmanager   # use with -WITH-
+    def w_locked(self):
+        try:
+            self.w_acquire()
+            yield
+        finally:
+            self.w_release()
