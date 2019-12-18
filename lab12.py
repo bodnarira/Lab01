9f459dbb399926d9179b01420af24e60c9d7f6b4
bodnarira <55149654+bodnarira@users.noreply.github.com>
2019-12-18 11:49:01 +0200

    Update

--- lab12.py
+++ lab12.py
@@ -3,20 +3,7 @@ import hashlib
 import threading
 import math
 
-
-thread_n = 20
-dir = "C:/Users/webvi/OneDrive/Documents/GitHub/"
-duplicates = {}
-files_list = []
-for d in os.walk(dir):
-    for i in d[2]:
-        files_list.append(d[0]+"/"+i)
-
-files_shift = math.ceil(len(files_list)/thread_n)
-files_start = -files_shift
-threads = []
-
-def get_duplicates (files_start, files_end, chunk_size):
+def get_duplicates (duplicates, files_list, files_start, files_end, chunk_size):
     for i in range(files_start, files_end):
         hasher = hashlib.md5()
         with open(files_list[i], 'rb') as file:
@@ -24,27 +11,43 @@ def get_duplicates (files_start, files_end, chunk_size):
         while len(buf) > 0:
             hasher.update(buf)
             buf = file.read(chunk_size)
-        file.close()
         data = hasher.hexdigest()
         if data in duplicates:
             duplicates[data].append(files_list[i])
         else:
             duplicates[data] = [files_list[i]]
+            
+def find_duplicates (dir, thread_n):
+    duplicates = {}
+    files_list = []
+    for d in os.walk(dir):
+        for i in d[2]:
+            files_list.append(d[0]+"/"+i)
 
-while True:
-    files_start += files_shift
-    files_end = files_start+files_shift
-    if files_end > len(files_list): 
-        files_end = len(files_list)
-    t = threading.Thread(target=get_duplicates, args=[files_start, files_end, 65536])
-    t.start()
-    threads.append(t)
-    if files_end == len(files_list):
-        break
+    files_shift = math.ceil(len(files_list)/thread_n)
+    files_start = -files_shift
+    threads = []
+    
+    while True:
+        files_start += files_shift
+        files_end = files_start+files_shift
+        if files_end > len(files_list): 
+            files_end = len(files_list)
+        t = threading.Thread(target=get_duplicates, args=[duplicates, files_list, files_start, files_end, 65536])
+        t.start()
+        threads.append(t)
+        if files_end == len(files_list):
+            break
 
-for t in threads:
-    t.join()
-
-for key in duplicates:
-  if len(duplicates[key]) > 1:
-    print(duplicates[key])
+    for t in threads:
+        t.join()
+    
+    result = []
+    for key in duplicates:
+      if len(duplicates[key]) > 1:
+        result.append(duplicates[key])
+        
+    return result
+        
+if __name__ == "__main__":
+    print(find_duplicates("../../../Irinka/", 25))
